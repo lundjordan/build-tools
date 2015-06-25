6719bd9110e8aaa569a4f9d3cd5088d99ba4afa7
Jordan Lund <jordan.l.lund@gmail.com>
2015-06-25 12:23:36 -0700

    lines up with new generic archiver relengapi changes, download and extraction still wip

--- buildfarm/utils/archiver_client.py
+++ buildfarm/utils/archiver_client.py
@@ -9,19 +9,21 @@
 # Target:        Python 2.7.x
 #
 """
-    calls relengapi archiver endpoint, downloads returned s3 url, and unpacks it locally
+    calls relengapi archiver, downloads returned s3 url, and unpacks it locally
 """
+import gzip
 
 import logging
-import shutil
 import os
 import random
 import tarfile
+from tempfile import TemporaryFile
 import time
 import urllib2
 import json
 
 from optparse import OptionParser
+import shutil
 
 SUCCESS_CODE = 0
 # This is not an infra error and we can't recover from it
@@ -30,13 +32,13 @@ FAILURE_CODE = 1
 # let sheriffs determine if re-triggering is needed
 INFRA_CODE = 3
 
-EXIT_CODE = FAILURE_CODE
-
-ENDPOINT_CONFIG = {
+ARCHIVER_CONFIGS = {
     'mozharness': {
-        'url_format': "archiver/{endpoint}/{rev}?repo={repo}&region={region}&suffix={suffix}",
-        'archive_root': "{repo}-{rev}",
-        'archive_subdir': "testing/mozharness",
+        'url_format': "archiver/hgmo/{repo}/{rev}?&preferred_region={region}&suffix={suffix}",
+        # the root path from within the archive
+        'extract_root_dir': "{repo}-{rev}",
+        # the subdir path from within the root
+        'default_extract_subdir': "testing/mozharness",
     }
 }
 
@@ -45,7 +47,9 @@ SUBDIR_ROOT = {
 }
 
 RELENGAPI_HOST = {
-    'staging': 'https://api-pub-build.allizom.org/',
+    # 'staging': 'https://api-pub-build.allizom.org/',
+    # TODO - delete this
+    'staging': 'http://127.0.0.1:8010/',
     'production': 'https://api.pub.build.mozilla.org/'
 }
 
@@ -89,7 +93,6 @@ def get_response_from_task(url, options):
     :return: response obj
     """
     task_result = {}
-    response = None
     for _ in retrier(attempts=options.max_retries, sleeptime=options.sleeptime,
                      max_sleeptime=options.max_retries * options.sleeptime):
         task_result = get_task_result(url)
@@ -101,14 +104,16 @@ def get_response_from_task(url, options):
         if task_result.get('s3_urls'):
             # grab a s3 url using the preferred region if available
             s3_url = task_result['s3_urls'].get(options.region, task_result['s3_urls'].values()[0])
-            response = urllib2.urlopen(s3_url)
+            print s3_url
+            return urllib2.urlopen(s3_url)
         else:
             log.error("An s3 URL could not be determined even though archiver task completed. Check"
                       "archiver logs for errors. Task status: {}".format(task_result['status']))
+            exit(FAILURE_CODE)
     else:
         log.error("Archiver's task could not be resolved. Check archiver logs for errors. Task "
                   "status: {}".format(task_result['status']))
-    return response
+        exit(FAILURE_CODE)
 
 
 def get_url_response(url, options):
@@ -131,133 +136,83 @@ def get_url_response(url, options):
             if response.code == 202:
                 # archiver is taking a long time so it started a sub task
                 response = get_response_from_task(response.info()['location'], options)
-                if not response:
-                    return INFRA_CODE
 
             if response.code == 200:
                 break
+            else:
+                log.debug("got a bad response. response code: {}".format(response.code))
 
-        except urllib2.HTTPError as e:
+        except (urllib2.HTTPError, urllib2.URLError) as e:
             if num == options.max_retries - 1:
-                log.exception("HTTPError Exception: {}".format(e))
-                return INFRA_CODE
-        except urllib2.URLError as e:
-            if num == options.max_retries - 1:
-                log.exception("URLError for {}. Exception: {}".format(url, e))
-                return INFRA_CODE
+                log.exception(e)
+                exit(INFRA_CODE)
         num += 1
 
     if not response.code == 200:
         content = response.read()
         log.error("could not determine a valid url response. return code: '{}'"
                   "return content: '{}".format(response.code, content))
-        return INFRA_CODE
+        exit(INFRA_CODE)
     return response
 
 
-def download_and_extract_archive(response, archive_name):
-    log.info("downloading archive: {}".format(archive_name))
+def get_tar_mode(content_type):
+    conversion_dict = {'application/x-gzip': 'r:gz',
+                       'application/x-bzip2': 'r:bz2'}
     try:
-        with open(archive_name, 'wb') as archivef:
-            archivef.write(response.read())
-    except IOError, e:
-        log.exception("IOError: {}".format(e))
-        return INFRA_CODE
+        return conversion_dict[content_type]
+    except KeyError:
+        log.error("Invalid content-type from url. "
+                  "Got: {} Valid types: {}".format(content_type, str(conversion_dict)))
+        exit(FAILURE_CODE)
 
-    log.info("extracting downloaded archive: {}".format(archive_name))
+
+def download_and_extract_archive(response, extract_root, destination):
+    content_type = None
     try:
-        with tarfile.open(archive_name) as tar:
-            tar.extractall()
+        content_type = response.headers['Content-Type']
+    except KeyError:
+        log.error("Couldn't determine compression type of archive. Missing 'Content-Type' header. "
+                  "Headers received: {}".format(str(response.headers.keys())))
+        exit(FAILURE_CODE)
+
+    try:
+        with tarfile.open(fileobj=response, mode=get_tar_mode(content_type)) as tar:
+            # only extract paths within the extract_root
+            for member in tar:
+                if not member.name.startswith(extract_root):
+                    continue
+                member.name = member.name.replace(extract_root, '')
+                tar.extract(member, destination)
     except tarfile.TarError as e:
-        log.exception("TarError: {}".format(e))
-        return INFRA_CODE
+        log.exception("Could not download and extract archive. See Traceback:")
+        log.exception(e)
+        exit(INFRA_CODE)
 
-    return SUCCESS_CODE
 
-
-def copy_archive_to_destination(source, destination):
+def archiver(url, config_key, options):
     """
-    copies extracted source contents of an archive into a destination path. If the destination
-    has relatively similiar path names, those destination paths will be overwritten.
-    """
-    if not os.path.exists(source):
-        log.error("Can't determine source directory where archive was extracted to. "
-                  "Expected: {}".format(source))
-        return INFRA_CODE
-    try:
-        if not os.path.exists(destination):
-            os.makedirs(destination)
-
-        log.info("Moving extracted path's contents: '{}'"
-                 "to destination: '{}'".format(source, destination))
-        for src_path in os.listdir(source):
-            abs_src_path = os.path.join(source, src_path)
-            abs_dest_path = os.path.join(destination, src_path)
-            if os.path.exists(abs_dest_path):
-                if os.path.isdir(abs_dest_path):
-                    shutil.rmtree(abs_dest_path)
-                else:
-                    os.remove(abs_dest_path)
-            shutil.move(abs_src_path, abs_dest_path)
-    except IOError, e:
-        log.exception("IOError: {}".format(e))
-        return INFRA_CODE
-    except shutil.Error as e:
-        log.exception("shutil.Error: {}".format(e))
-        return INFRA_CODE
-
-    return SUCCESS_CODE
-
-
-def clean_up(archive_path, extracted_root_source):
-    """
-    cleans up archive and extracted archive source.
-    """
-    log.info("Cleaning up archive and archive remnants.")
-    log.info("Removing paths '{}' and '{}".format(archive_path, extracted_root_source))
-    try:
-        os.remove(archive_path)
-        shutil.rmtree(extracted_root_source)
-    except IOError, e:
-        log.exception("IOError: {}".format(e))
-        return INFRA_CODE
-    except shutil.Error as e:
-        log.exception("shutil.Error: {}".format(e))
-        return INFRA_CODE
-
-    return SUCCESS_CODE
-
-
-def get_archive(url, endpoint, options):
-    """
-    1) obtains valid s3 url for archive via relengapi's archiver endpoint
+    1) obtains valid s3 url for archive via relengapi's archiver
     2) downloads and extracts archive
-    3) merges contents of extracted archive into destination, overwriting duplicate paths
-    4) cleans up archive and leftover extracted paths
     """
-    archive_cfg = ENDPOINT_CONFIG[endpoint]  # specifics to the the archiver endpoint
-    archive_path = "{}.tar.gz".format(endpoint)  # filename of downloaded archive
-    # determine the extracted archive source and dest paths
-    extracted_root_source = archive_cfg['archive_root'].format(repo=os.path.basename(options.repo),
-                                                               rev=options.rev)
-    if archive_cfg.get('archive_subdir'):
-        extracted_full_source = os.path.join(extracted_root_source, archive_cfg['archive_subdir'])
-    else:
-        extracted_full_source = extracted_root_source
-    extracted_destination = options.destination or os.path.join(os.getcwd(), endpoint)
+    archive_cfg = ARCHIVER_CONFIGS[config_key]  # specifics to the archiver endpoint
 
     response = get_url_response(url, options)
+    for h in response.headers:
+        print 'header: {}, {}'.format(h, response.headers[h])
+    print response.code
 
-    return_code = download_and_extract_archive(response, archive_path)
+    # get the root path within the archive that will be the starting path of extraction
+    subdir = options.subdir or archive_cfg.get('default_extract_subdir')
+    extract_root = archive_cfg['extract_root_dir'].format(
+        repo=os.path.basename(options.repo), rev=options.rev
+    )
+    if subdir:
+        extract_root = os.path.join(extract_root, subdir)
 
-    if return_code == SUCCESS_CODE:
-        return_code = copy_archive_to_destination(source=extracted_full_source,
-                                                  destination=extracted_destination)
+    destination = options.destination or os.path.join(os.getcwd(), config_key)
 
-        if return_code == SUCCESS_CODE:
-            return_code = clean_up(archive_path, extracted_root_source)
-
-    return return_code
+    download_and_extract_archive(response, extract_root, destination)
 
 
 def options_args():
@@ -271,6 +226,9 @@ def options_args():
                       help="The revision the archive is based on.")
     parser.add_option("--region", dest="region", default='us-west-2',
                       help="The preferred region of the s3 archive.")
+    parser.add_option("--subdir", dest="subdir",
+                      help="The path within the root of the archive of where to start extracting "
+                           "from. If not supplied, the entire archive will be extracted.")
     parser.add_option("--destination", dest="destination",
                       help="The path location of where to extract the archive to.")
     parser.add_option("--staging", dest='staging', action='store_true', default=False,
@@ -289,31 +247,37 @@ def options_args():
     options, args = parser.parse_args()
 
     if not len(args) == 1:
-        parser.error("archiver_client.py requires exactly 1 argument: the archiver endpoint. "
-                     "Valid endpoints: {}".format(str(ENDPOINT_CONFIG.keys())))
+        parser.error("archiver_client.py requires exactly 1 argument: the archiver config. "
+                     "Valid configs: {}".format(str(ARCHIVER_CONFIGS.keys())))
 
     if options.debug:
         log.setLevel(logging.DEBUG)
         log.info("Setting DEBUG logging.")
+
+    config = args[0]
+    if not ARCHIVER_CONFIGS.get(config):
+        log.error("Config argument is unknown. "
+                  "Given: '{}', Valid: {}".format(config, str(ARCHIVER_CONFIGS.keys())))
+        exit(FAILURE_CODE)
 
     return options, args
 
 
 def main():
     options, args = options_args()
-    endpoint = args[0]
-
-    if not ENDPOINT_CONFIG.get(endpoint):
-        log.error("endpoint argument is unknown. "
-                  "Given: '{}', Valid: {}".format(endpoint, str(ENDPOINT_CONFIG.keys())))
-        exit(FAILURE_CODE)
+    config = args[0]
 
     api_url = RELENGAPI_HOST['staging' if options.staging else 'production']
-    api_url += ENDPOINT_CONFIG[endpoint]['url_format'].format(
-        endpoint=endpoint, rev=options.rev, repo=options.repo, region=options.region, suffix='tar.gz'
+    api_url += ARCHIVER_CONFIGS[config]['url_format'].format(
+        rev=options.rev, repo=options.repo, region=options.region, suffix='tar.gz'
     )
+    subdir = options.subdir or ARCHIVER_CONFIGS[config].get('default_extract_subdir')
+    if subdir:
+        api_url += "&subdir={}".format(subdir)
 
-    exit(get_archive(url=api_url, endpoint=endpoint, options=options))
+    archiver(url=api_url, config_key=config, options=options)
+
+    exit(SUCCESS_CODE)
 
 
 if __name__ == '__main__':
